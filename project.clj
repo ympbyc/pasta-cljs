7331f2b0183d2f2a5a298c3ba850c167f3e9a022
ympbyc <ympbyc@gmail.com>
2014-02-22 19:40:44 -0800

    macros

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
-(defproject pasta-cljs "0.1.1-SNAPSHOT"
+(defproject pasta-cljs "0.1.1"
   :description "App as a value"
   :url "http://github.com/ympbyc/pasta-cljs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins      [[lein-cljsbuild "0.3.2"]]
+  :cljsbuild {:builds [{:source-paths ["src"]
+                        :compiler {:output-to "resources/public/js/main.js"
+                                   :optimizations :none
+                                   :pretty-print true}}]})
---
+++ resources/public/js/main.js
@@ -0,0 +1,3 @@
+goog.addDependency("base.js", ['goog'], []);
+goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.format', 'goog.string.StringBuffer']);
+goog.addDependency("../pasta_cljs/core.js", ['pasta_cljs.core'], ['cljs.core']);
